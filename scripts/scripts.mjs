1dd273a125021634f2830bc11d4b839b419f79b5
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-05-23 01:33:01 -0700

    DOM objects

---
+++ scripts/scripts.mjs
@@ -0,0 +1,6 @@
+const gList = document.getElementById("groceryList");
+// console.log(gList);
+const gInput = document.getElementById("groceryInput");
+// console.log(gInput);
+const gBtn = document.getElementById("addGroceryBtn");
+// console.log(gBtn);
